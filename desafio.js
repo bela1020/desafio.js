ccda798d75ee4d2ced134fe28117c753a2185af9
bela1020 <isabela.santim@escola.pr.gov.br>
2025-11-05 21:12:46 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,12 @@
+//Console.log("Meu primeiro programa: trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+    let idade = 16;
+console.log("idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+    let idadeSomada = 18;
+console.log(idadeSomada)
+
+
